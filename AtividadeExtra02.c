1af9904725817941f1361136113f04063438b36c
edugui-19 <eduardamgui19@gmail.com>
2025-04-30 19:39:35 -0300

    Update: Tabulação

--- AtividadeExtra02.c
+++ AtividadeExtra02.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int dist, tempo, velocidade;
+	int dist, tempo, velocidade;
 	char partida[30], chegada[30];
 	
 /*Apuração de informações*/
 printf("Em que cidade você começou sua viagem?\n");
-    scanf("%s", &partida);
+	scanf("%s", &partida);
 printf("Qual foi o seu destino?\n");
-    scanf("%s", &chegada);
+	scanf("%s", &chegada);
 printf("Qual a distância entre essas cidades?\n");
-    scanf("%i", &dist);
+	scanf("%i", &dist);
 printf("Por último, quanto tempo durou sua viagem?\n");
-    scanf("%i", &tempo);
+	scanf("%i", &tempo);
     
 /*Cálculo e resposta*/
 velocidade = dist/tempo;
