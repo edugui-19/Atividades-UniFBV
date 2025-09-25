56edc8fa56a7a8463e3039c909e3c71c6f7103bc
edugui-19 <eduardamgui19@gmail.com>
2025-09-25 10:38:58 -0300

    Delete contato.php

--- contato.php
+++
@@ -1,5 +0,0 @@
-<?php include __DIR__ . '/header.php'; ?>
-
-<h2>Contato</h2>
-<?php include __DIR__ . '/footer.php'; ?>
- <img class="responsive-img" src="https://i.ytimg.com/vi/EjqT2rhsmWU/mqdefault.jpg"
